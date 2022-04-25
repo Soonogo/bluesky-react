d8df3ccf8b2ec1cbb8029224f9af47f5995caf0e
Songsong <614750803@qq.com>
2022-04-26 00:18:58 +0800

    v1

--- scripts/depoly.sh
+++ scripts/depoly.sh
@@ -4,6 +4,6 @@ cd build &&
 git init &&
 git add . &&
 git commit -m 'deploy' &&
-git remote add origin git@github.com:songenen/bs-website.git &&
+git remote add origin git@github.com:songenen/mo-web.git &&
 git push -u origin master -f &&
 cd -
