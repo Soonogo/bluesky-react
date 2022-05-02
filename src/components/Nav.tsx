3edbe2b8bf16c17dd4b6ed01433e8ae8574c5f1b
Songsong <614750803@qq.com>
2022-05-02 22:30:05 +0800

    replace icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Icon from "./Icon";
 
 const NavWrapper = styled.nav`
-  line-height: 24px;
+  line-height: 16px;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
 
   > ul {
